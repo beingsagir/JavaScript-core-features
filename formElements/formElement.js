9fe79579a536984e553873c756224428ba9e3746
Sagiruddin Mondal <sagir.sagir@gmail.com>
2015-05-01 15:11:02 +0530

    form element functionalities

---
+++ formElements/formElement.js
@@ -0,0 +1,5 @@
+function doIt(input){
+	var input = document.getElementById("inputValue").value;
+	console.log(input);
+	document.getElementById("result").innerHTML = "<h5>But I am happy now cause you have typed : </h5>" + input;
+}
